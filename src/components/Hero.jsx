0839fb457cf961eee86af2945ea2160305c96150
kenan551 <kenan.ramazanov1104@gmail.com>
2023-10-15 11:45:40 +0400

    Creating Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import  Container  from './Container'
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section id='Home' className="overflow-hidden py-20 sm:py-32 lg:pb-32 xl:pb-3">
+<Container></Container>
+    </section>
+  )
+};
+
+export default Hero
