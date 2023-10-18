7846e7e23be2f9984072ccbe5eb24590b2cdcfe6
kenan551 <kenan.ramazanov1104@gmail.com>
2023-10-18 21:05:35 +0400

    Final of Account section

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,7 +1,56 @@
+import {
+  DeviceArrowIcon,
+  DeviceCardsIcon,
+  DeviceClockIcon,
+  DeviceListIcon,
+  DeviceLockIcon,
+  DeviceChartIcon,
+} from "@/components/StockLogos";
+
+
+
+
 export const navData = [
     { _id: 101, title: "Home", href: "#home" },
     { _id: 102, title: "Features", href: "#features" },
     { _id: 103, title: "Account", href: "#account" },
     { _id: 104, title: "Reviews", href: "#reviews" },
   ];
-  
+  export const accountData = [
+    {
+      name: "Invest any amount",
+      description:
+        "Whether it is $1 or $1,000,000, we can put your money to work for you.",
+      icon: DeviceArrowIcon,
+    },
+    {
+      name: "Build a balanced profile",
+      description:
+        "Invest in different industries to find the most opportunities to win huge.",
+      icon: DeviceCardsIcon,
+    },
+    {
+      name: "Trade in real-time",
+      description:
+        "Get insider tips on big stock moves and act on them within seconds.",
+      icon: DeviceClockIcon,
+    },
+    {
+      name: "Profit from your network",
+      description:
+        "Invite new insiders to get tips faster and beat even other Investa users.",
+      icon: DeviceListIcon,
+    },
+    {
+      name: "Encrypted and anonymized",
+      description:
+        "Cutting-edge security technology that even the NSA does not know about keeps you hidden.",
+      icon: DeviceLockIcon,
+    },
+    {
+      name: "Progress tracking",
+      description:
+        "Watch your investments grow exponentially, leaving other investors in the dust.",
+      icon: DeviceChartIcon,
+    },
+  ];
