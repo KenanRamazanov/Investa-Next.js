4fb1f971bee37e58f2fef6e0fd14522d4499d400
kenan551 <kenan.ramazanov1104@gmail.com>
2023-10-18 21:11:25 +0400

    Creating Reviews

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@
 import Account from '@/components/Account'
 import Feature from '@/components/Feature'
 import Hero from '@/components/Hero'
+import Reviews from '@/components/Reviews'
 import Image from 'next/image'
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
 <Hero/>
 <Feature/>
 <Account/>
+<Reviews/>
    </main>
   )
 }
